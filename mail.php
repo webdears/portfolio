5cffeb6c6c58cd9e1d57ea5da90abdc7d001e2f7
webdears <92512312+webdears@users.noreply.github.com>
2022-02-24 01:49:18 +0530

    new mail file

--- mail.php
+++ mail.php
@@ -1,93 +1,123 @@
 <?php
+
 $firstname=$_POST["firstname"];
 $lastname=$_POST["lastname"];
 $email=$_POST["mail"];
 $mob=$_POST["mobile"];
 $subject=$_POST["subject"];
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-//Include required PHPMailer files
-	require 'includes/PHPMailer.php';
-	require 'includes/SMTP.php';
-	require 'includes/Exception.php';
-//Define name spaces
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\SMTP;
-	use PHPMailer\PHPMailer\Exception;
-//Create instance of PHPMailer
-	$mail = new PHPMailer();
-//Set mailer to use smtp
-	$mail->isSMTP();
-//Define smtp host
-	$mail->Host = "smtp.gmail.com";
-//Enable smtp authentication
-	$mail->SMTPAuth = true;
-//Set smtp encryption type (ssl/tls)
-	$mail->SMTPSecure = "tls";
-//Port to connect smtp
-	$mail->Port = "587";
-//Set gmail username
-	$mail->Username = "webdearsproject@gmail.com";
-//Set gmail password
-	$mail->Password = "web@1234";
-//Email subject
-	$mail->Subject = "Test email using PHPMailer";
-//Set sender email
-	$mail->setFrom('webdearsproject@gmail.com');
-//Enable HTML
-	$mail->isHTML(true);
-//Attachment
-	// $mail->addAttachment('images/1.2.jpg');
-//Email body
-	$mail->Body = "<h1>{$firstname}{$lastname}</h1></br><p>{$email}</p></br><p>{$subject}</p></br><p>{$mob}</p>";
-//Add recipient
-	$mail->addAddress('webdearsproject@gmail.com');
-//Finally send email
-	if ( $mail->send() ) {
-		echo customer($email,$firstname);
-	}else{
-		echo $mail->ErrorInfo;
-	}
-//Closing smtp connection
-	$mail->smtpClose();
- 
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+// try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'webdearsproject@gmail.com';                     //SMTP username
+    $mail->Password   = 'web@1234';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('webdearsproject@gmail.com', 'Web-dears');
+    $mail->addAddress('webdearsproject@gmail.com');     //Add a recipient
+    // $mail->addAddress('web-dears');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('images/THANK-YOU.jpg');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject =  "Test email using PHPMailer";
+    $mail->Body    =  "<h1>{$firstname}{$lastname}</h1></br><p>{$email}</p></br><p>{$subject}</p></br><p>{$mob}</p>";
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if($mail->send()){
+
+        echo customer($email,$firstname); 
+    }
+    // echo 'Message has been sent';
+    else{
+        echo $mail->ErrorInfo;
+    }
+    $mail->smtpClose();
+// } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
 function customer($email,$name){
-	
-	$mail = new PHPMailer();
-//Set mailer to use smtp
-	$mail->isSMTP();
-//Define smtp host
-	$mail->Host = "smtp.gmail.com";
-//Enable smtp authentication
-	$mail->SMTPAuth = true;
-//Set smtp encryption type (ssl/tls)
-	$mail->SMTPSecure = "tls";
-//Port to connect smtp
-	$mail->Port = "587";
-//Set gmail username
-	$mail->Username = "webdearsproject@gmail.com";
-//Set gmail password
-	$mail->Password = "web@1234";
-//Email subject
-	$mail->Subject = "REQUEST RECEIVED";
-//Set sender email
-	$mail->setFrom('webdearsproject@gmail.com');
-//Enable HTML
-	$mail->isHTML(true);
-//Attachment
-	$mail->addAttachment('images/THANK-YOU.jpg');
-//Email body
-	$mail->Body = "<h1>WEB DEARS</h1></br><h3>Hi {$name},</h3></br><p>You have connected with right people ,THANK YOU for choosing us.</p></br><p>WE WILL GET BACK TO AS SOON AS POSSIBLE.</p>";
-//Add recipient
-	$mail->addAddress($email);
-//Finally send email
-	if ($mail->send()) {
-		// return "request suuccessful";
-		return 1;
-	}else{
-		// echo $mail->ErrorInfo;
-		// return "error occoured";
-		return 0;
-	}
-//Closing smtp connection
-	$mail->smtpClose();
+
+    $mail = new PHPMailer(true);
+
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'webdearsproject@gmail.com';                     //SMTP username
+    $mail->Password   = 'web@1234';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('webdearsproject@gmail.com', 'Web-dears');
+    $mail->addAddress($email);     //Add a recipient
+    // $mail->addAddress('web-dears');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('images/THANK-YOU.jpg');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject =  "Request Received";
+    $mail->Body = "<h1>WEB DEARS</h1></br><h3>Hi {$name},</h3></br><p>You have connected with right people ,THANK YOU for choosing us.</p></br><p>WE WILL GET BACK TO AS SOON AS POSSIBLE.</p>";
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if($mail->send()){
+
+        return 1;
+    }
+    // echo 'Message has been sent';
+    else{
+        return 0;
+    
+    }
+
+    $mail->smtpClose();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
