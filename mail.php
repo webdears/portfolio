9dbb9e71c6e75c32b6257699b6f01a8e87da92e4
webdears <92512312+webdears@users.noreply.github.com>
2022-02-23 23:56:36 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -24,7 +24,7 @@ $subject=$_POST["subject"];
 //Set smtp encryption type (ssl/tls)
 	$mail->SMTPSecure = "tls";
 //Port to connect smtp
-	$mail->Port = "588";
+	$mail->Port = "587";
 //Set gmail username
 	$mail->Username = "webdearsproject@gmail.com";
 //Set gmail password
