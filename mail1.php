b06bb67cbb94f9fbd75543a881cab283c2a577d2
webdears <92512312+webdears@users.noreply.github.com>
2022-07-31 13:32:48 +0530

    Update mail1.php

--- mail1.php
+++ mail1.php
@@ -21,10 +21,10 @@ try {
     //Server settings
 //     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'ssl://smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'webdearsproject@gmail.com';                     //SMTP username
-    $mail->Password   = 'web@1234';                               //SMTP password
+    $mail->Password   = 'xajdbwfjvheiedtp';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
@@ -64,10 +64,10 @@ function customer($email,$name){
 
 //     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'ssl://smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'webdearsproject@gmail.com';                     //SMTP username
-    $mail->Password   = 'web@1234';                               //SMTP password
+    $mail->Password   = 'xajdbwfjvheiedtp';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
